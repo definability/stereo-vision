3914e3ca8b0dd09874ceb21d03c0ae939775bf8e
char-lie <5962274+char-lie@users.noreply.github.com>
2018-02-28 22:41:51 +0200

    Close #5: Implement matrix class to represent images
    Implement the `Matrix` class with ability to create a zero matrix of specific size, read and write particular elements.

---
+++ src/matrix.hpp
@@ -0,0 +1,57 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <vector>
+#include <utility>
+#include <stdexcept>
+
+using std::vector;
+using std::move;
+using std::invalid_argument;
+
+/**
+ * \brief The Matrix is a storage for pixels of images.
+ *
+ * Images may be grayscale,
+ * contain RGB or RGBA color channels,
+ * that's why a template parameter T is needed.
+ */
+template<typename T> class Matrix {
+    private:
+        vector<vector<T> > data;
+        size_t rows_;
+        size_t columns_;
+    public:
+        Matrix(const Matrix& matrix) = default;
+        Matrix(Matrix&& matrix) = default;
+        Matrix(size_t rows, size_t columns) {
+            this->rows_ = rows;
+            this->columns_ = columns;
+            this->data = vector< vector<T> >(rows);
+            for (vector<T>& row : this->data) {
+                row = vector<T>(columns);
+            }
+        }
+        ~Matrix() = default;
+
+        size_t columns() const {
+            return this->columns_;
+        }
+
+        size_t rows() const {
+            return this->rows_;
+        }
+
+        /**
+         * \brief An ability to read and write colors in particular pixels.
+         *
+         * Don't rely on a vector type of the output hardly.
+         * Use it only to read and write particular pixels,
+         * because behavior may change in future.
+         */
+        vector<T>& operator[](size_t index) {
+            return this->data[index];
+        }
+};
+
+#endif
---
+++ tests/unit/testmatrix.hpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "matrix.hpp"
+
+class MatrixTest : public ::testing::Test {
+    protected:
+        MatrixTest() {};
+        virtual ~MatrixTest() {};
+        virtual void SetUp() {};
+        virtual void TearDown() {};
+};
