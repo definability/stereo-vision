4ca8c93898cb68f18f2534de2565867648e7051c
char-lie <5962274+char-lie@users.noreply.github.com>
2018-03-08 22:13:41 +0200

    Implement value parameterized unit tests to check non-existent edges penalties

---
+++ tests/unit/testdisparity_graph_inf_edges.hpp
@@ -0,0 +1,17 @@
+#include <tuple>
+
+#include "gtest/gtest.h"
+#include "disparity_graph.hpp"
+
+using std::tuple;
+
+class DisparityGraphInfiniteEdgesTest
+        : public ::testing::TestWithParam<
+              tuple<DisparityNode, DisparityNode>
+        > {
+    protected:
+        DisparityGraphInfiniteEdgesTest() {};
+        virtual ~DisparityGraphInfiniteEdgesTest() {};
+        virtual void SetUp() {};
+        virtual void TearDown() {};
+};
