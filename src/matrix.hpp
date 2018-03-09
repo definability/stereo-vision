f961314e1a26875ae59ca78540ce42bcf37f17d8
char-lie <5962274+char-lie@users.noreply.github.com>
2018-03-09 20:34:53 +0200

    Use member initializer list in Matrix constructor

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -16,17 +16,17 @@ using std::invalid_argument;
  */
 template<typename T> class Matrix {
     private:
-        vector<vector<T> > data;
         size_t rows_;
         size_t columns_;
+        vector<vector<T> > data_;
     public:
         Matrix(const Matrix& matrix) = default;
         Matrix(Matrix&& matrix) = default;
-        Matrix(size_t rows, size_t columns) {
-            this->rows_ = rows;
-            this->columns_ = columns;
-            this->data = vector< vector<T> >(rows);
-            for (vector<T>& row : this->data) {
+        Matrix(size_t rows, size_t columns)
+                : rows_{rows}
+                , columns_{columns}
+                , data_{vector< vector<T> >(rows)} {
+            for (vector<T>& row : this->data_) {
                 row = vector<T>(columns);
             }
         }
@@ -48,7 +48,7 @@ template<typename T> class Matrix {
          * because behavior may change in future.
          */
         vector<T>& operator[](size_t index) {
-            return this->data[index];
+            return this->data_[index];
         }
 };
 
