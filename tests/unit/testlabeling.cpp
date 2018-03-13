cb584ad9bf9c5847a5a8810f511299aa12c84da8
char-lie <5962274+char-lie@users.noreply.github.com>
2018-03-13 21:10:47 +0200

    Add unit tests to check nodes set and penalty calculation of labeling

--- tests/unit/testlabeling.cpp
+++ tests/unit/testlabeling.cpp
@@ -20,7 +20,7 @@ TEST(LabelingTest, RightAvailableDisparities) {
     Labeling<unsigned char> labeling{graph};
 
     vector<size_t> disparities;
-    
+
     disparities = labeling.nodeDisparities({9, 9});
     ASSERT_EQ(disparities.size(), 1);
     ASSERT_EQ(disparities.front(), 0);
@@ -43,6 +43,20 @@ TEST(LabelingTest, SetNodeSuccess) {
     ASSERT_EQ(labeling.disparity({0, 0}), 0);
 }
 
+TEST(LabelingTest, SetNodesSuccess) {
+    Matrix<unsigned char> left{10, 10}, right{10, 10};
+    DisparityGraph<unsigned char> graph{left, right};
+    Labeling<unsigned char> labeling{graph};
+
+    ASSERT_EQ(labeling.disparity({5, 5}), 0);
+    labeling.setNode({5, 5, 1});
+    ASSERT_EQ(labeling.disparity({5, 5}), 1);
+    labeling.setNode({5, 4, 2});
+    ASSERT_EQ(labeling.disparity({5, 4}), 2);
+    labeling.setNode({5, 3, 3});
+    ASSERT_EQ(labeling.disparity({5, 3}), 3);
+}
+
 TEST(LabelingTest, PenaltyRight) {
     Matrix<unsigned char> left{10, 10}, right{10, 10};
     DisparityGraph<unsigned char> graph{left, right};
@@ -51,4 +65,11 @@ TEST(LabelingTest, PenaltyRight) {
     ASSERT_DOUBLE_EQ(labeling.penalty(), 0);
     labeling.setNode({0, 0, 1});
     ASSERT_DOUBLE_EQ(labeling.penalty(), 2);
+    labeling.setNode({5, 5, 1});
+    ASSERT_DOUBLE_EQ(labeling.penalty(), 2 + 4 * 1);
+    labeling.setNode({5, 4, 2});
+    ASSERT_DOUBLE_EQ(labeling.penalty(), 2 + 4 * 1 + 3 * 4);
+    labeling.setNode({5, 3, 3});
+    ASSERT_DOUBLE_EQ(labeling.penalty(), 2 + 4 * 1 + 2 * 4 + 1 * 1
+                                       + 3 * 9);
 }
