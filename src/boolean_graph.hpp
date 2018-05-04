480a34c32a7e12d305a9ae70a09ffa2d12f8df19
char-lie <5962274+char-lie@users.noreply.github.com>
2018-05-04 21:32:17 +0300

    Refactor boolean graph and add documentation

--- src/boolean_graph.hpp
+++ src/boolean_graph.hpp
@@ -13,13 +13,41 @@ using std::none_of;
 using std::pair;
 using std::vector;
 
+/**
+ * \brief Boolean graph is needed to perform removing procedure.
+ *
+ * To check whether the problem is solved,
+ * we need to check whether we've found its trivial analogue.
+ * If after removal of bad edges we will have a labeling,
+ * the problem is solved.
+ */
 template<typename Color> class BooleanGraph {
     private:
+        /**
+         * \brief Disparity graph to build boolean graph for.
+         */
         const DisparityGraph<Color>& graph_;
+        /**
+         * \brief Cached nodes for performance needs.
+         */
         vector <DisparityNode> nodes_;
+        /**
+         * \brief Nodes of the boolean graph
+         * contain availability of an original graph nodes.
+         */
         vector< vector<bool> > nodesAvailability_;
+        /**
+         * \brief Edges of the boolean graph
+         * contain availability of an original graph edges.
+         */
         vector< vector< vector< vector<bool> > > > edgesAvailability_;
-
+        /**
+         * \brief Check whether the edge between two nodes is availabe.
+         *
+         * The edge is available if its value is `true`
+         * and its nodes are also available.
+         * Such iterative check allows to get availability of any labeling.
+         */
         bool edgeAvailable_(
             const DisparityNode& node,
             const DisparityNode& neighbor)
@@ -40,14 +68,22 @@ template<typename Color> class BooleanGraph {
                 return this->edgeAvailable_(neighbor, node);
             }
         }
-
+        /**
+         * \brief Mark specific node as unavailable.
+         */
         void removeNode_(const DisparityNode& node)
         {
             size_t nodeIndex = node.row * this->graph_.columns() + node.column;
             this->nodesAvailability_[nodeIndex][node.disparity] = false;
             for (DisparityNode neighbor : this->graph_.nodeNeighbors(node))
             {
-                for (size_t neighborDisparity = this->graph_.minNeighborDisparity(node, neighbor); neighborDisparity < this->graph_.maxNeighborDisparity(node, neighbor); ++neighborDisparity)
+                size_t minDisparity = this->graph_.minNeighborDisparity(
+                    node, neighbor);
+                size_t maxDisparity = this->graph_.maxNeighborDisparity(
+                    node, neighbor);
+                for (size_t neighborDisparity = minDisparity;
+                     neighborDisparity < maxDisparity;
+                     ++neighborDisparity)
                 {
                     neighbor.disparity = neighborDisparity;
                     this->removeEdge(node, neighbor);
@@ -55,7 +91,9 @@ template<typename Color> class BooleanGraph {
                 }
             }
         }
-
+        /**
+         * \brief Go through each vertex and edge and remove unavailable ones.
+         */
         bool deletionIteration_()
         {
             bool changed = false;
@@ -64,7 +102,11 @@ template<typename Color> class BooleanGraph {
             for (DisparityNode node: this->nodes_)
             {
                 graphExists = false;
-                for (size_t disparity = this->graph_.minDisparity(node); disparity < this->graph_.maxDisparity(node); ++disparity)
+                size_t minDisparity = this->graph_.minDisparity(node);
+                size_t maxDisparity = this->graph_.maxDisparity(node);
+                for (size_t disparity = minDisparity;
+                     disparity < maxDisparity;
+                     ++disparity)
                 {
                     node.disparity = disparity;
                     if (!this->nodeAvailable(node))
@@ -75,7 +117,13 @@ template<typename Color> class BooleanGraph {
                        : this->graph_.nodeNeighbors(node))
                     {
                         bool nodeAvailable = false;
-                        for (size_t neighborDisparity = this->graph_.minNeighborDisparity(node, neighbor); neighborDisparity < this->graph_.maxNeighborDisparity(node, neighbor); ++neighborDisparity)
+                        size_t minDisparity = this->graph_.minNeighborDisparity(
+                            node, neighbor);
+                        size_t maxDisparity = this->graph_.maxNeighborDisparity(
+                            node, neighbor);
+                        for (size_t neighborDisparity = minDisparity;
+                             neighborDisparity < maxDisparity;
+                             ++neighborDisparity)
                         {
                             neighbor.disparity = neighborDisparity;
                             if (this->edgeAvailable_(node, neighbor))
@@ -101,7 +149,11 @@ template<typename Color> class BooleanGraph {
             {
                 for (DisparityNode node: this->nodes_)
                 {
-                    for (size_t disparity = this->graph_.minDisparity(node); disparity < this->graph_.maxDisparity(node); ++disparity)
+                    size_t minDisparity = this->graph_.minDisparity(node);
+                    size_t maxDisparity = this->graph_.maxDisparity(node);
+                    for (size_t disparity = minDisparity;
+                         disparity < maxDisparity;
+                         ++disparity)
                     {
                         node.disparity = disparity;
                         this->removeNode_(node);
@@ -112,7 +164,9 @@ template<typename Color> class BooleanGraph {
 
             return changed;
         }
-
+        /**
+         * \brief Allocated all needed data and prepare initial availability.
+         */
         void initialize_()
         {
             this->nodesAvailability_.clear();
@@ -173,24 +227,48 @@ template<typename Color> class BooleanGraph {
             }
         }
     public:
+        /**
+         * \brief We don't need default constructor.
+         */
         BooleanGraph() = delete;
+        /**
+         * \brief Initialize the boolean graph based on disparity graph.
+         */
         BooleanGraph(const DisparityGraph<Color>& graph)
             : graph_{graph}
             , nodes_{graph.availableNodes()}
         {
             this->initialize_();
         }
+        /**
+         * \brief Copy constructor is default.
+         */
         BooleanGraph(const BooleanGraph&) = default;
+        /**
+         * \brief Move constructor is default.
+         */
         BooleanGraph(BooleanGraph&&) = default;
+        /**
+         * \brief Destructor is default.
+         */
         ~BooleanGraph() = default;
-
+        /**
+         * \brief Refresh graph data.
+         *
+         * Basic initialization was made in initialize_() method,
+         * and this one should be used before each new removal procedure.
+         */
         void initialize()
         {
             for (DisparityNode node: this->nodes_)
             {
                 size_t nodeIndex =
                     node.row * this->graph_.columns() + node.column;
-                for (size_t disparity = this->graph_.minDisparity(node); disparity < this->graph_.maxDisparity(node); ++disparity)
+                size_t minDisparity = this->graph_.minDisparity(node);
+                size_t maxDisparity = this->graph_.maxDisparity(node);
+                for (size_t disparity = minDisparity;
+                     disparity < maxDisparity;
+                     ++disparity)
                 {
                     node.disparity = disparity;
                     assert(
@@ -208,7 +286,13 @@ template<typename Color> class BooleanGraph {
                             this->edgesAvailability_
                                 [nodeIndex][disparity].size()
                             > neighborIndex);
-                        for (size_t neighborDisparity = this->graph_.minNeighborDisparity(node, neighbor); neighborDisparity < this->graph_.maxNeighborDisparity(node, neighbor); ++neighborDisparity)
+                        size_t minDisparity = this->graph_.minNeighborDisparity(
+                            node, neighbor);
+                        size_t maxDisparity = this->graph_.maxNeighborDisparity(
+                            node, neighbor);
+                        for (size_t neighborDisparity = minDisparity;
+                             neighborDisparity < maxDisparity;
+                             ++neighborDisparity)
                         {
                             neighbor.disparity = neighborDisparity;
                             this->edgesAvailability_
@@ -221,13 +305,20 @@ template<typename Color> class BooleanGraph {
                 }
             }
         }
+        /**
+         * \brief Check whether available labeling exists.
+         */
         bool isFinished()
         {
             while(this->deletionIteration_());
 
             for (DisparityNode node : this->nodes_)
             {
-                for (size_t disparity = this->graph_.minDisparity(node); disparity < this->graph_.maxDisparity(node); ++disparity)
+                size_t minDisparity = this->graph_.minDisparity(node);
+                size_t maxDisparity = this->graph_.maxDisparity(node);
+                for (size_t disparity = minDisparity;
+                     disparity < maxDisparity;
+                     ++disparity)
                 {
                     node.disparity = disparity;
                     if (this->nodeAvailable(node))
@@ -238,7 +329,9 @@ template<typename Color> class BooleanGraph {
             }
             return false;
         }
-
+        /**
+         * \brief Mark edge as unavailable.
+         */
         void removeEdge(
             const DisparityNode& node,
             const DisparityNode& neighbor)
@@ -274,7 +367,9 @@ template<typename Color> class BooleanGraph {
                 this->removeEdge(neighbor, node);
             }
         }
-
+        /**
+         * \brief Check node availability.
+         */
         bool nodeAvailable(const DisparityNode& node)
         {
             return this->nodesAvailability_
